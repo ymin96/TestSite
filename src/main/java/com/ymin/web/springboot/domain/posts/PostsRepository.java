8a85aed60cfa7bffb0e3ac240adbc90326c45822
Kim YongHwan <ymin96@naver.com>
2020-04-13 15:22:02 +0900

    chapter.4 complete

--- src/main/java/com/ymin/web/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/ymin/web/springboot/domain/posts/PostsRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
 
-    @Query("SELECT P FROM Posts p ORDER BY p.id DESC")
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
 }
