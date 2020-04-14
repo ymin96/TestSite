c912863c373ca6a1b1a34db195904be8c69827df
Kim YongHwan <ymin96@naver.com>
2020-04-14 16:05:09 +0900

    p.205

--- src/main/java/com/ymin/web/springboot/web/IndexController.java
+++ src/main/java/com/ymin/web/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.ymin.web.springboot.web;
 
+import com.ymin.web.springboot.config.auth.LoginUser;
+import com.ymin.web.springboot.config.auth.dto.SessionUser;
 import com.ymin.web.springboot.service.posts.PostsService;
 import com.ymin.web.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +20,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
