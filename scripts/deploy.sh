770f293ee9d2c11f9c8509c736b33666fa9d6b1e
Kim YongHwan <ymin96@naver.com>
2020-04-21 16:26:21 +0900

    update project name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=springboot2-webservice
+PROJECT_NAME=TestSite
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl TestSite | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
